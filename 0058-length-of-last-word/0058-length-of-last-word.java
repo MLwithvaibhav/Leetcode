a7f11aaabe072f3df01f73edf1825fbb1eaa8cb6
Vaibhav Sharma <thevaibhavsharma17@gmail.com>
2026-09-07 16:24:51 +0530

    Time: 0 ms (100%), Space: 43.2 MB (32.3%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -4,12 +4,12 @@ class Solution {
         int i = n - 1;
         int length = 0;
 
-        // 1. Peeche ke trailing spaces skip karein
+        
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
 
-        // 2. Last word ke characters count karein
+       
         while (i >= 0 && s.charAt(i) != ' ') {
             length++;
             i--;
